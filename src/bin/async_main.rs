309e5f526850f8ebe30895eecca59cb23ea13d41
Salsa Steve <elmanstevelaguna@gmail.com>
2024-12-21 06:42:29 +0000

    i2s working with sensor unverified data

--- src/bin/async_main.rs
+++ src/bin/async_main.rs
@@ -1,38 +1,81 @@
+//! This shows how to continuously receive data via I2S.
+//!
+//! Without an additional I2S source device you can connect 3V3 or GND to DIN
+//! to read 0 or 0xFF or connect DIN to WS to read two different values.
+//!
+//! You can also inspect the MCLK, BCLK and WS with a logic analyzer.
+//!
+//! The following wiring is assumed:
+//! - MCLK =>  GPIO0 (not supported on ESP32)
+//! - BCLK =>  GPIO2
+//! - WS   =>  GPIO4
+//! - DIN  =>  GPIO5
+
+//% CHIPS: esp32 esp32c3 esp32c6 esp32h2 esp32s2 esp32s3
+//% FEATURES: embassy embassy-generic-timers
+
 #![no_std]
 #![no_main]
 
-use defmt::info;
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
-use esp_hal::prelude::*;
 use {defmt_rtt as _, esp_backtrace as _};
+use esp_hal::{
+    dma::{Dma, DmaPriority},
+    dma_buffers,
+    i2s::master::{DataFormat, I2s, Standard},
+    prelude::*,
+    timer::timg::TimerGroup,
+};
+use defmt::info;
 
-extern crate alloc;
+#[esp_hal_embassy::main]
+async fn main(_spawner: Spawner) {
+    info!("Init!");
+    let peripherals = esp_hal::init(esp_hal::Config::default());
 
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_hal_embassy::init(timg0.timer0);
 
-#[main]
-async fn main(spawner: Spawner) {
-    let peripherals = esp_hal::init({
-        let mut config = esp_hal::Config::default();
-        config.cpu_clock = CpuClock::max();
-        config
-    });
+    let dma = Dma::new(peripherals.DMA);
+    let dma_channel = dma.channel0;
 
-    esp_alloc::heap_allocator!(72 * 1024);
+    let (rx_buffer, rx_descriptors, _, tx_descriptors) = dma_buffers!(4092 * 4, 0);
 
-    let timer0 = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER)
-        .split::<esp_hal::timer::systimer::Target>();
-    esp_hal_embassy::init(timer0.alarm0);
+    let i2s = I2s::new(
+        peripherals.I2S0,
+        Standard::Philips,
+        DataFormat::Data16Channel16,
+        44100u32.Hz(),
+        dma_channel.configure(false, DmaPriority::Priority0),
+        rx_descriptors,
+        tx_descriptors,
+    )
+    .into_async();
 
-    info!("Embassy initialized!");
+    let i2s = i2s.with_mclk(peripherals.GPIO0);
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
+    let i2s_rx = i2s
+        .i2s_rx
+        .with_bclk(peripherals.GPIO2)
+        .with_ws(peripherals.GPIO4)
+        .with_din(peripherals.GPIO5)
+        .build();
 
+    let buffer = rx_buffer;
+    info!("Start");
+
+    let mut data = [0u8; 5000];
+    let mut transaction = i2s_rx.read_dma_circular_async(buffer).unwrap();
     loop {
-        info!("Hello world!");
-        Timer::after(Duration::from_secs(1)).await;
-    }
+        let avail = transaction.available().await.unwrap();
+        info!("available {}", avail);
 
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.22.0/examples/src/bin
+        let count = transaction.pop(&mut data).await.unwrap();
+        info!(
+            "got {} bytes, {:02x}..{:02x}",
+            count,
+            &data[..10],
+            &data[count - 10..count]
+        );
+    }
 }

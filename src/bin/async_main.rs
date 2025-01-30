29d50836e944a9b1fbed6d6483692623a5bcd465
Salsa Steve <elmanstevelaguna@gmail.com>
2025-01-30 04:55:33 +0000

    Compiles and does hann window on samples

--- src/bin/async_main.rs
+++ src/bin/async_main.rs
@@ -23,11 +23,34 @@ use esp_hal::{
     prelude::*,
     timer::timg::TimerGroup
 };
+use esp_alloc as _;
 use {defmt_rtt as _, esp_backtrace as _};
+use micro_dsp::normalize_samples;
+use microdsp::common::apply_window_function;
+use core::mem::MaybeUninit;
+
+
+fn init_heap() {
+    const HEAP_SIZE: usize = 32 * 1024;
+    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
+
+    unsafe {
+        esp_alloc::HEAP.add_region(esp_alloc::HeapRegion::new(
+            HEAP.as_mut_ptr() as *mut u8,
+            HEAP_SIZE,
+            esp_alloc::MemoryCapability::Internal.into(),
+        ));
+    }
+}
+
+
+const BYTES_PER_SAMPLE: usize = 2;
+const FFT_SIZE: usize = 1024;
 
 #[esp_hal_embassy::main]
 async fn main(_spawner: Spawner) {
     info!("Init!");
+    init_heap();
 
     let peripherals = esp_hal::init({
         let mut config = esp_hal::Config::default();
@@ -78,7 +101,27 @@ async fn main(_spawner: Spawner) {
 
         let count = transaction.pop(&mut data).await.unwrap();
 
-        info!("count {}", count);
+        // get the first 1024 samples if they are available
+        if count >= FFT_SIZE * BYTES_PER_SAMPLE {
+            let mut samples = [0i16; FFT_SIZE];
+            samples
+                .iter_mut()
+                .zip(data.chunks_exact(2))
+                .for_each(|(sample, chunk)| {
+                    *sample = i16::from_le_bytes([chunk[0], chunk[1]]);
+                });
 
+            let mut f32_samples: [f32; FFT_SIZE] = [0.0; FFT_SIZE];
+            normalize_samples(&samples, &mut f32_samples);
+
+            info!("Samples: {:?}", &f32_samples[0..5]);
+
+            let window_function = microdsp::common::WindowFunctionType::Hann;
+            apply_window_function(window_function, &mut f32_samples);
+   
+            info!("Samples: {:?}", &f32_samples[0..5]);
+
+            
+        }
     }
 }
